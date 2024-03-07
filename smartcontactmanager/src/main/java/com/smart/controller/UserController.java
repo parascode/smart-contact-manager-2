adfaf19ff76b8a661ad4d765cd7e9cfc1a8e34cb
parascode <jparas435@gmail.com>
2024-03-07 18:39:08 +0530

    added sidebar

--- smartcontactmanager/src/main/java/com/smart/controller/UserController.java
+++ smartcontactmanager/src/main/java/com/smart/controller/UserController.java
@@ -1,18 +1,30 @@
 package com.smart.controller;
 
+import java.security.Principal;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.smart.dao.UserRepository;
+import com.smart.entities.User;
 
 @Controller
 @RequestMapping("/user")
 //@EnableWebSecurity
 public class UserController {
+	
+	@Autowired
+	private UserRepository userRepository;
 
 	@RequestMapping("/index")
 //	@PreAuthorize("hasAuthority('ROLE_USER')")
-	public String index() {
+	public String index(Model m, Principal principal) {
+		User user = userRepository.getUserByUserName(principal.getName());
+		m.addAttribute("user", user);
 		return "normal/user_dashboard";
 	}
 }
--- smartcontactmanager/src/main/resources/static/js/script.js
+++ smartcontactmanager/src/main/resources/static/js/script.js
@@ -1,4 +1,17 @@
 /**
- * 
+ *
  */
-console.log("This is script file running")
+console.log("This is script file running");
+const toggleSidebar = () => {
+  if ($(".sidebar").is(":visible")) {
+    //if opened then
+    // write code to close
+    $(".sidebar").css("display", "none");
+    $(".content").css("margin-left", "2%");
+  } else {
+    // if closed
+    // write code to open
+    $(".sidebar").css("display", "block");
+    $(".content").css("margin-left", "20%");
+  }
+};
